a32f210886b3ada1d636d54a517d5e15478910e6
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-11-05 14:03:04 +0200

    Redirect to home page
    Redirect to Home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import Home from '../pages/Home';
 // import { Catalog } from 'pages/Catalog';
 // import { Favorites } from 'pages/Favorites';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Layout from './Layout/Layout';
 
-export const App = () => {
+export const App = () => {  
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         {/* <Route path="catalog" element={<Catalog />} /> */}
         {/* <Route path="favorites" element={<Favorites />} /> */}
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,16 @@
 import React, { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { Container } from '../Container/Container';
 import { Header, NavList, CurrentLink } from './Layout.styled';
 import logo192 from '../../components/UI/img/logo192.png';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate('/');
+  };
+
   return (
     <>
       <Header>
@@ -22,7 +28,7 @@ const Layout = () => {
               src={logo192}
               alt="cars"
               style={{ height: 75, marginRight: 20, cursor: 'pointer' }}
-              to="/"             
+              onClick={goToHome}
             />
             <NavList>
               <CurrentLink to="/">Home</CurrentLink>
